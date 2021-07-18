b497987b351fa7c4cb81d6e8ef1d2eec26b31da8
Oszlányi Kornél <koszlanyi777@gmail.com>
2021-07-18 22:29:17 +0200

    cors handling

--- src/pages/api/projects.ts
+++ src/pages/api/projects.ts
@@ -1,6 +1,26 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 
+const allowCors = (fn: any) => async (req: any, res: any) => {
+	res.setHeader("Access-Control-Allow-Credentials", true);
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	// another common pattern
+	// res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+	res.setHeader(
+		"Access-Control-Allow-Methods",
+		"GET,OPTIONS,PATCH,DELETE,POST,PUT"
+	);
+	res.setHeader(
+		"Access-Control-Allow-Headers",
+		"X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+	);
+	if (req.method === "OPTIONS") {
+		res.status(200).end();
+		return;
+	}
+	return await fn(req, res);
+};
+
 type Data = {
 	name: string;
 	id: number;
@@ -10,7 +30,7 @@ type SuccesData = {
 	succes: boolean;
 };
 
-export default async function handler(
+async function Handler(
 	req: NextApiRequest,
 	res: NextApiResponse<Data[] | SuccesData>
 ) {
@@ -27,3 +47,5 @@ export default async function handler(
 			return res.status(200).json({ succes: false });
 	}
 }
+
+export default allowCors(Handler);
